4b58e0efa41f88fa724005483ba68f3f7cf54c3d
Branden Arnold <brandena@zillowgroup.com>
2016-08-02 19:44:28 -0700

    Removed extra layer of json when loading

--- neighborhood_hackweek/local/neighborhood_data.py
+++ neighborhood_hackweek/local/neighborhood_data.py
@@ -2,5 +2,5 @@ import json
 
 def get_neighborhood_data_by_region_id_neighborhood(region_id_neighborhood):
     with open('neighborhood_data/' + str(region_id_neighborhood) + '.json') as json_file:
-        json_data = json.load(json_file)
+        json_data = json.load(json_file)['neighborhood_data']
     return json_data
--- neighborhood_hackweek/local/property_region.py
+++ neighborhood_hackweek/local/property_region.py
@@ -2,5 +2,5 @@ import json
 
 def get_property_regions_by_region_id_neighborhood(region_id_neighborhood):
     with open('property_region/' + str(region_id_neighborhood) + '.json') as json_file:
-        json_data = json.load(json_file)
+        json_data = json.load(json_file)['property_regions']
     return json_data
